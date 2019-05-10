e9cb9fd6a10bf80ef19436c17275d59cd68371c3
John Florence <codytdrew@gmail.com>
2019-05-10 15:50:21 -0700

    letter test.  when you type a letter from the character object, it is displayed back in the console.

--- letter.js
+++ letter.js
@@ -2,7 +2,6 @@
 This constructor should be able to either display an underlying character or 
 a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. 
 That means the constructor should define:
-    A string value to store the underlying character for the letter
     A boolean value that stores whether that letter has been guessed yet
     A function that returns the underlying character if the letter has been guessed, or a placeholder 
     (like an underscore) if the letter has not been guessed
@@ -10,3 +9,14 @@ That means the constructor should define:
     updating the stored boolean value to true if it was guessed correctly 
     --Letter.js should not require any other files.
     */
+
+var testLetter = new Letter('a');
+
+function Letter(character) {
+    // A string value to store the underlying character for the letter
+    this.character = character;
+    // display character
+    // display underScore
+    console.log('gucci');
+};
+console.log(testLetter.character);
